86cbfcd4e251353200660923b3088e8170789909
Scott Colson <scott@colsoncode.com>
2018-05-19 10:03:50 -0700

    Clean up folders

--- TelemetryInterfaces/Telemetry/IEventTelemetry.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace CCLCC.Telemetry.Interfaces
-{
-    public interface IEventTelemetry : ITelemetry, IDataModelTelemetry<IEventDataModel>, ISupportProperties, ISupportMetrics, ISupportSampling
-    {
-        string Name { get; }
-    }
-}
